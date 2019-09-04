aa520d745e47a538f8ef4694f291d561ca0b77ca
Marco Aurélio Guimarães <marcocpdti@gmail.com>
2019-09-03 23:43:12 -0300

    my second commit of curse oministack

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
-
+import ScheduleController from './app/controllers/ScheduleController';
 import multerconfig from './config/multer';
 
 const routes = new Router();
@@ -14,12 +14,12 @@ const upload = multer(multerconfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionControler.store);
-
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
-
+routes.get('/schedule', ScheduleController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
