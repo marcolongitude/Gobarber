e7e9eda5ad0e9495dee41a014f3d927c33d75481
Marco Aurélio Guimarães <marcocpdti@gmail.com>
2019-09-08 21:39:23 -0300

    testingPackageGitAtom
    Co-Authored-By: MarcoAurelio <marcocpdti@gmail.com>

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,3 +1,4 @@
+//controller para agendamento do sistema
 import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
@@ -31,15 +32,16 @@ class AppointmentController {
     });
     return res.json(appointments);
   }
-
-  async store(req, res) {
+    async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
       date: Yup.date().required(),
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation fails!' });
+      return res.status(400).json({
+        error: 'Validation fails!'
+      });
     }
 
     const { provider_id, date } = req.body;
